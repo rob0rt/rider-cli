4f146cd0408dae03f0c96130b40ea3cf6ba9c945
Robert Lynch <robertlynch2012@gmail.com>
2015-09-05 21:04:17 -0400

    Move namespacing around

--- templates/lib/Render.php
+++ templates/lib/Render.php
@@ -1,9 +1,13 @@
 <?hh // strict
 
-class Rider\Render {
-  public static function go(:xhp $content, ?string $controller): void {
+namespace Rider;
+
+use :rider:layout;
+
+class Render {
+  public static function go(\XHPRoot $content, ?string $controller): void {
     print
-      <rider:layout js-files={Rider\JS::get_required()} css-files={Rider\CSS::get_required()}>
+      <rider:layout js-files={JS::get_required()} css-files={CSS::get_required()}>
         {$content}
       </rider:layout>;
   }
